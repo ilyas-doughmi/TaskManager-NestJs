d10c215b46eb7a544da736b257d181b2f4fd46ea
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 12:37:56 +0000

    feat: adding new function to create Task and push it into Tasks Array then return it

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+ 
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];
@@ -8,5 +9,17 @@ export class TasksService {
     public getAllTasks(): Task[]
     {
         return this.tasks;
+    }
+    
+    public createTask(title:string, description:string): Task
+    {
+        const task: Task = {
+            id: uuid(),
+            title: title,
+            description: description,
+            status : TaskStatus.OPEN,
+        }
+        this.tasks.push(task);
+        return task;
     }
 }
